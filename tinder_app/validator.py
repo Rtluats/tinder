3f1449973d263969467039eb3ff91a71e9d8b649
Rtluats <rtluats@gmail.com>
2021-03-02 11:04:01 +0300

    override perform_update

--- tinder_app/validator.py
+++ tinder_app/validator.py
@@ -12,19 +12,3 @@ def validate_date(value: datetime.date):
             _('Invalid date = %(date), you must be older to be on this site'),
             params={'date': value},
         )
-
-def validate_update_cords(user):
-    valid_hours = datetime.datetime.now().hour - 2
-    valid_datetime = datetime.datetime.now()
-    valid_datetime = valid_datetime.replace(hour=valid_hours)
-    if user.last_cords_update is not None and user.last_cords_update > valid_datetime:
-        raise ValidationError(
-            _("User can't update his cords, because 2 hours have not passed yet"),
-        )
-
-
-def validate_counter_swipes(user):
-    if user.group.number_of_allowed_swipes != - 1 and user.group.number_of_allowed_swipes <= user.counter_swipes:
-        raise ValidationError(
-            _("the user can't make more likes"),
-        )
--- tinder_app/views.py
+++ tinder_app/views.py
@@ -1,10 +1,13 @@
+import datetime
+
 from django.contrib.gis.geos import GEOSGeometry
 from django.db.models import Q
 from rest_framework import generics
 from rest_framework import permissions
+from rest_framework.exceptions import ValidationError
 
 from tinder_app.models import (
-    User, Like
+    User,
 )
 from .serializers import UserSerializer
 
@@ -35,7 +38,9 @@ class UserListView(generics.ListAPIView):
         if user.distance_look != -1:
             geo_us1 = GEOSGeometry(user.cords)
             users_by_distance = [u for u in users_by_distance.all()
-                                 if user.distance_look >= int(geo_us1.distance(GEOSGeometry(u.cords)))]
+                                 if (user.group.allowed_distance == -1
+                                     or user.group.allowed_distance >= user.distance_look) and
+                                 user.distance_look >= int(geo_us1.distance(GEOSGeometry(u.cords)))]
 
         return users_by_distance[:10]
 
@@ -46,8 +51,14 @@ class UserDetailView(generics.RetrieveUpdateDestroyAPIView):
     permission_classes = [permissions.IsAuthenticated]
 
     def perform_update(self, serializer):
-        pass
+        user = User.objects.get(pk=int(serializer.data["id"]))
 
-
-
-
+        if user.block_disable is not None and user.block_disable <= datetime.datetime.now():
+            if user.group.number_of_allowed_swipes != -1 and user.counter_swipes == user.group.number_of_allowed_swipes:
+                day = datetime.datetime.now().day + 1
+                block = datetime.datetime.now()
+                block = block.replace(day=day)
+                user.block_disable = block
+                user.save()
+        elif user.block_disable is not None:
+            raise ValidationError(f"user can't swipe until {user.block_disable}")
