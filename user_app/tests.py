256eaba656c65146b10fed8f97318ac37574b1ab
Rtluats <rtluats@gmail.com>
2021-03-12 21:47:54 +0300

    add some tests

--- user_app/tests.py
+++ user_app/tests.py
@@ -11,9 +11,21 @@ def test_user_create():
     User.objects.create(name='John', email='user@email.com', password='password', birth_date=date.replace(year=2002))
     assert User.objects.count() == 1
 
-#
-# @pytest.mark.django_db
-# def test_view(client):
-#     url = reverse('homepage-url')
-#     response = client.get(url)
-#     assert response.status_code == 200
+
+@pytest.mark.django_db
+def test_view_non_auth(client):
+    url = reverse('user-list')
+    response = client.get(url)
+    assert response.status_code == 401
+
+
+@pytest.mark.django_db
+def test_view_auth(client):
+    date = datetime.date.today()
+    User.objects.create(name='John', email='user@email.com', password='password', birth_date=date.replace(year=2002))
+    url = '/auth/'
+    client.login(
+        username='user@email.com', passwd='password'
+    )
+    response = client.get(url)
+    assert response.status_code == 200
